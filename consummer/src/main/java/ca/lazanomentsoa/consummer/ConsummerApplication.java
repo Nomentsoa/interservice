6fefc3f39ac999f577479729244f6548694d3c15
Laza_Nomentsoa_Sedera <starksedera@gmail.com>
2026-08-18 14:38:55 -0400

    restclient

--- consummer/src/main/java/ca/lazanomentsoa/consummer/ConsummerApplication.java
+++ consummer/src/main/java/ca/lazanomentsoa/consummer/ConsummerApplication.java
@@ -1,7 +1,9 @@
 package ca.lazanomentsoa.consummer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class ConsummerApplication {
